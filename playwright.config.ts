3b4c251050ed0a75346030c28e22a1926cbe80c6
Kirill <rywes@yandex.ru>
2025-11-03 02:53:51 +0300

    config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,9 +5,7 @@ import { defineConfig, devices } from '@playwright/test';
  * https://github.com/motdotla/dotenv
  */
 import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
-
+dotenv.config();
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
