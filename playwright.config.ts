8bb123314f51b6e15c07bbbf1c377876550fe452
Kirill <rywes@yandex.ru>
2025-11-03 04:58:50 +0300

    2525

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
     // { name: 'setup', testMatch: /setup\/global\.setup\.ts/ },
 
     {
-      name: 'Автоматизация интеграций с МИС',
+      name: 'Интеграции с МИС',
       use: { ...devices['Desktop Chrome'] },
       //dependencies: ['setup'],
     },
